88ecee3eaa0ab85756f8a707a3663b22a976e22e
KoalaNovice <94567518+KoalaNovice@users.noreply.github.com>
2022-01-24 12:30:34 -0800

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Player
+[
+    private String name;
+    private int points;
+
+    public Player()
+    {
+        System.out.println("Enter Player Name: ");
+        Scanner sc = new 
+    }
+]
